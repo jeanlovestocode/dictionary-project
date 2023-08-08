9ff369f78d23285027dec2c4036df21b7cc3a535
jeanlovestocode <edemjeantori5@gmail.com>
2023-08-08 14:54:36 -0400

    imported react files and added logo
    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./dict.png"
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="dictionary-logo img-fluid"  alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+
