a61597313233f75d6a36d6b34546241e921315b5
jeanlovestocode <edemjeantori5@gmail.com>
2023-08-10 15:15:15 -0400

    created synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props){
 
@@ -6,8 +7,13 @@ export default function Meaning(props){
     return (
       <div className="Meaning">
         <h4>{props.meaning.partOfSpeech}</h4>
-        <p>{props.meaning.definition}</p>
+        <p>
+            <strong>Definition: </strong>
+            {props.meaning.definition}  
+        </p>
         <p>{props.meaning.example}</p>
+
+        <Synonyms synonyms={props.meaning.synonyms}/>
       </div>
     );
 }
